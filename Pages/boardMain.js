eeeeeb10887ec880164b9676d3bdfbfa65919e68
김보성 <ghalsru@naver.com>
2019-05-02 17:13:20 +0900

    Change : 게시판 수정. boardMain으로 통합, 및 세부 페이지 추가

--- Pages/boardMain.js
+++ Pages/boardMain.js
@@ -1,12 +1,7 @@
 
 var Observable = require("FuseJS/Observable");
 
-var date = new Date();
-var content = [""]
-content=Observable([]);
 
-var maxSimulatedDelay = 1.5
-var minSimulatedDelay = 0.25
 
 var opts = ({
 	id : '2',
@@ -14,85 +9,93 @@ var opts = ({
 });
 
 function write(){
-	id = '1';
+	_id = '1';
 	router.push('write',opts);
-	// console.log(opts.id, opts.name)
 
 }
+
+var maxSimulatedDelay = 1.5
+var minSimulatedDelay = 0.25
+
 
 var _id = Observable("");
 var category = Observable("");
 var name = Observable("");
 var title = Observable("");
 var array = Observable("");
-var result = Observable("");
+var content = Observable("");
+var days = Observable("");
 
-exports.items = Observable();
+var items = Observable();
 
-exports.isLoading = Observable(false);
+// exports.isLoading = Observable(false);
+var isLoading = Observable(false);
 
 
-exports.loadMore = function(){
-	//it's possible this gets called again before the previous loading is complete
 
-	if (exports.isLoading.value) {
+svp.check();
+function loadMore(){
+	if (isLoading.value) {
 		return
 	}
-
-	exports.isLoading.value = true;
+	isLoading.value = true;
 
 	var delay = Math.random() * (maxSimulatedDelay - minSimulatedDelay) + minSimulatedDelay
 	setTimeout( loadSome, delay * 1000 );
 	
 }
 
-
-exports.loadSome=function(){
+function loadSome(){
 	fetch('http://18.222.99.74/board/am',{
-	// fetch('http://aa52f6e2.ngrok.io/board/am',{
-		method: "GET",
-		headers: {
-			"Content-type": "application/JSON"
-		}
+		// fetch('http://aa52f6e2.ngrok.io/board/am',{
+			method: "GET",
+			headers: {
+				"Content-type": "application/JSON"
+			}
 
 
-	}).then(function(result){
-		result = result;
-		exports.result = result;
-		return result.json();
-	}).then(function(result){
-		for(var i in result){
-			exports.items.add( {
-				name: result[i].name,
-				_id : result[i]._id,
-				title : result[i].title
-			})
-			// console.log(name.value);				
-		}
+		}).then(function(res){
+			return res.json();
+		}).then(function(res){
 
-	}).catch((err)=>{
-		console.log(err);
-	});
+			for(var i in res){
 
-	exports.isLoading.value = false;
+				items.add(createPage(res[i].name,res[i].title,res[i].content,res[i].date)
 
-	svp.check();
+				);		
+				console.log(res);
 
-}
+			}
+
+			isLoading.value = false;
+
+		}).catch((err)=>{
+			console.log(err);
+		});
+	}
+
+// for문과 합치게 되면 마지막 데이터만 들어가는 문제 발생하기 때문에 반드시 createPage로 분리 시켜줘야한다.
+	function createPage(name, title, content, date) {
+		return {
+			name : name,
+			title: title,
+			content : content,
+			days : date,
+			gotoDetails: function() {
+				router.push("detailPage", { title: title , name:name, content:content, days:date})
+			}
+		};
+	}
+
+	exports.clicked= function(){router.goto("SubPage")}
 
 
+	module.exports = {
 
-
-module.exports = {
-	
-	content : content,
-	write: write,
-	opts : opts,
-	result:result,
-
-	page1: function() { router.goto("page1"); },
-	page2: function() { router.goto("page2"); },
-	page3: function() { router.goto("page3"); }
-};
+		items : items,
+		isLoading : isLoading,
+		write : write,
+		loadMore : loadMore
+	};
 
 
