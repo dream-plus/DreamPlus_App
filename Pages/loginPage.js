f817837cdc4016ce056ad16b063c0ff28788ac31
김보성 <ghalsru@naver.com>
2019-05-26 20:50:18 +0900

    Change : 주석 추가 및 변수명 변경

--- Pages/loginPage.js
+++ Pages/loginPage.js
@@ -4,35 +4,36 @@ var ID = Observable("");
 var Password = Observable("");
 var NetworkError = Observable(false);
 var Loading = Observable(false);
-var autoLoin = Observable(false);
+var autoLogin = Observable(false);
 
-
+// 서버와 연동이 안되었을 때 다시 시도하는 함수.
 function retry(){
 	NetworkError.value = false;
 	Session();
 }
 
+// 자동로그인을 클릭 했을 때 실행되는 함수
 function autoCheck(){
-	autoLoin.value = true;
+	autoLogin.value = true;
 }
 
+// 자동로그인을 클릭 취소 했을 때 실행되는 함수
 function autoClose(){
-	autoLoin.value = false;
+	autoLogin.value = false;
 }
 
 function SignIn(){
 
+	// username, password 변수는 로그인 할때 id와 pw를 나타냄
+	// autoLogin은 자동로그인 부분을 체크 했는지 안했는지 구분하기 위한 변수(체크 하면 true)
     var opts = ({
             	'username' : ID.value,
-                'password' : Password.value
+                'password' : Password.value,
+                'autoLogin' : autoLogin.value
               });
-
-	console.log(JSON.stringify(opts));
-    console.log('sign'); //Sign_in 함수가 호출되었는지 확인
  
-
-	// fetch('http://18.222.99.74/users/signin',{
-		fetch('http://3ff05a06.ngrok.io/users/signin',{
+	fetch('http://18.222.99.74/users/signin',{
+		// fetch('http://3ff05a06.ngrok.io/users/signin',{
 	            method: "POST",
 	            headers: {
 	            	"Content-type": "application/JSON"
@@ -40,71 +41,63 @@ function SignIn(){
 	            body : JSON.stringify(opts)
 	               
 	        }).then((res)=>{
-	            // console.log(JSON.stringify(res));
-	            // console.log(JSON.parse(res));
 	            return res.json()
 			}).then((res)=>{
 
-	            console.log(res.success);
-
 	            if( JSON.parse(res.success) == true){
-	            	// router.push("Home", ID);
-	            	 // ID = ID.value;
 	            	router.push("home");
-	            	console.log("Move to MainPage");
-	            	// console.log(ID);
 	            }
-	            // JSON.parse(res._bodyInit).documents[1].address_name
 	        }).catch((err)=>{
 	            console.log(err);
 	        });
 
 }
 
+// 세션을 불러오는 함수.( 초기에 네트워크와 연결이 되었는지 확인하는 용도로도 쓰인다. )
 function Session(){
 
-	// fetch('http://18.222.99.74/users/session',{
-		fetch('http://3ff05a06.ngrok.io/users/session',{
+	fetch('http://18.222.99.74/users/session',{
+		// fetch('http://3ff05a06.ngrok.io/users/session',{
 			method: "GET",
 			headers: {
 				"Content-type": "application/JSON"
 			}
-
-
 		}).then(function(res){
 			return res.json();
 		}).then(function(res){
-			// console.log("session result = " + res.message);
-			if(res.message == 'true'){ 
-				console.log("test loading");
+			// message true는 세션이 작동 되었는지 확인하는 변수.
+			// autoLogin은 자동로그인 버튼을 누르면 true , 안누르면 false
+			if(res.message == 'true' && res.autoLogin == true){ 
 				Loading.value = true;
 				startLoading();
-				// router.push("home");
 			}			
 
 		}).catch((err)=>{
 			console.log(err );
 			if(err == "TypeError: Network request failed" ){
 				NetworkError.value = true;
-			}else if(err == "SyntaxError: Unexpected token < in JSON at position 1"){
+			}else if(err == "SyntaxError: Unexpected token < in JSON at position 1"){ // 이 SyntaxError는 서버에서 값을 받지 못할때 발생하므로 network failed와 같은 에러로 취급하였다.
 				NetworkError.value = true;
 			}
 			
 		});
 }
 
-Session();
+Session(); // Session 함수를 loginPage에 올때 무조건 실행시키기 위한 곳.
 
+// 로딩 화면 보여주는 함수.
 function startLoading() {
 		isBusy.activate();
 		setTimeout(function() {
 			isBusy.deactivate();
-			router.push("home");
+			Loading.value = false;
+			router.push("home"); // 로팅 후 홈으로 
 		}, 2000);
 
 
 	};
 
+// 회원가입 페이지로 이동하는 함수.
 function SignUp(){
 	router.push("signupPage");
 	console.log("Move to SignUp");
@@ -119,8 +112,7 @@ module.exports = {
 	NetworkError : NetworkError,
 	retry : retry,
 	Loading : Loading,
-	// startLoading : startLoading,
 	autoCheck : autoCheck,
-	autoLoin : autoLoin,
+	autoLogin : autoLogin,
 	autoClose : autoClose
 };
--- Pages/signupPage.js
+++ Pages/signupPage.js
@@ -1,10 +1,6 @@
 var Observable = require('FuseJS/Observable');
-
-function goBack()
-{
-	router.goBack();
-}
-
+var cameraRoll = require('FuseJS/CameraRoll');
+// var picture = Observable('Images/Camera.png');
 
 var newID = Observable("");
 var newPW = Observable("");
@@ -15,6 +11,9 @@ var newPhoneNum = Observable("");
 var ruleToggle = Observable(false);
 var majorToggle = Observable(false);
 var idCheck = Observable(false);
+var photoView = Observable(false);
+
+var imagePath = Observable("");
 
 var majorInit = Observable("융합 IT");
 var majorValue = ["융합 IT", "융합 디자인"];
@@ -40,14 +39,28 @@ function idpopup(){
 	idCheck.value = false;
 }
 
+function addImage(){
+	cameraRoll.getImage()
+	.then(function(image) {
+        imagePath.value = image.path;
+        photoView.value = true;
+    }, function(error) {
+        // 유저가 선택을 중단했을 경우, 또는 에러가 발생했을 경우에 호출됩니다.
+    });
+}
+
+
+
+
+
 function Save(){
 
 	fetch('http://18.222.99.74/users/signup',{
 		// fetch('http://3ff05a06.ngrok.io/users/signup',{
-		method: "POST",
-		headers: {
-			"Content-type": "application/json"
-		},
+			method: "POST",
+			headers: {
+				"Content-type": "application/json"
+			},
 		// body에 회원가입 정보를 넣어 서버에 보내는 부분.
 		body : JSON.stringify({
 			'_id' : newID.value,
@@ -62,37 +75,45 @@ function Save(){
 	}).then((res)=>{
 		// res 변수로 서버로부터 요청한 값을 받는 부분.
 		console.log(res.result);
-	            if( JSON.parse(res.success) == true){
-	            	router.push("loginPage");
-	            	console.log("Move to LoginPage");
-	            }else if (res.result == "overlapping"){
-	            	idCheck.value = true;
-	            }
+		if( JSON.parse(res.success) == true){
+			router.push("loginPage");
+			console.log("Move to LoginPage");
+		}else if (res.result == "overlapping"){
+			idCheck.value = true;
+		}
 
-	        }).catch((err)=>{
-	        	console.log(err);
-	        });
+	}).catch((err)=>{
+		console.log(err);
+	});
 
 
-	    }
+}
 
-	    module.exports = {
-	    	newID : newID,
-	    	newPW : newPW,
-	    	newName : newName,
-	    	newGender : newGender,
-	    	newNumber : newNumber,
-	    	newPhoneNum : newPhoneNum,
-	    	Save : Save,
-	    	goBack : goBack,
-	    	ruleOpen : ruleOpen,
-	    	ruleClose : ruleClose,
-	    	ruleToggle : ruleToggle,
-	    	majorInit : majorInit,
-	    	majorToggle : majorToggle,
-	    	majorChange : majorChange,
-	    	choicemajor : choicemajor,
-	    	majorValue : majorValue,
-	    	idCheck : idCheck,
-	    	idpopup : idpopup
-	    };
+function goBack()
+{
+	router.goBack();
+}
+
+module.exports = {
+	newID : newID,
+	newPW : newPW,
+	newName : newName,
+	newGender : newGender,
+	newNumber : newNumber,
+	newPhoneNum : newPhoneNum,
+	Save : Save,
+	goBack : goBack,
+	ruleOpen : ruleOpen,
+	ruleClose : ruleClose,
+	ruleToggle : ruleToggle,
+	majorInit : majorInit,
+	majorToggle : majorToggle,
+	majorChange : majorChange,
+	choicemajor : choicemajor,
+	majorValue : majorValue,
+	idCheck : idCheck,
+	idpopup : idpopup,
+	addImage : addImage,
+	imagePath : imagePath,
+	photoView : photoView
+};
